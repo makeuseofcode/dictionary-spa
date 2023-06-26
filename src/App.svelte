3e469268261b4cf992e76e8bf13c4e5029e207c2
David Uzondu (swtbtl) <46460041+daviduzondu@users.noreply.github.com>
2023-06-26 05:38:39 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Route, Router, Link } from "svelte-routing";
+  import Home from "./components/Home.svelte";
+  import Meaning from "./components/Meaning.svelte";
+</script>
+
+<Router>
+  <main class="app">
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+    <Route path="/" component={Home} />
+
+    <Route path="/find/:word" let:params>
+      <Meaning word={params.word} />
+    </Route>
+  </main>
+</Router>
---
+++ src/components/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { navigate } from "svelte-routing";
+  let enteredWord;
+</script>
+
+<main class="homepage">
+  <div class="dictionary-text">Dictionary</div>
+  <form on:submit|preventDefault={() => navigate(`/find/${enteredWord.toLowerCase()}`)}>
+    <input
+      type="text"
+      bind:value={enteredWord}
+      placeholder="Begin your search..."
+      autofocus
+    />
+    {#if enteredWord}
+      <button type="submit">Search Word</button>
+    {/if}
+  </form>
+</main>
